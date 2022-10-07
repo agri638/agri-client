cfe599d997089c36e7a2b090df1b6620e5dd63c3
Rahul Bhatt <bhattrahul270@gmail.com>
2022-10-08 02:04:12 +0530

    creating project structure and configuring deps

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import ProductDetails from "./components/product-details";
+function App() {
+  return (
+      <h1>Ths is my Project</h1>
+      // <ProductDetails id={1}/>
+  );
+}
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      ".src/**/*.tsx",
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
